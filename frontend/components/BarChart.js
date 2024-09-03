8cd43d7faeefaf76eb82cf62dbf81c826bbef987
Matthieu Recugnat <matthieu.recugnat@gmail.com>
2024-09-03 21:38:56 +1000

    fixing issue with ChartJS Line import

--- frontend/components/BarChart.js
+++ frontend/components/BarChart.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, LineController, LineElement, BarElement, Title, Tooltip, Legend } from 'chart.js';
+// import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ChartDataLabels);
+ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, LineController, Title, Tooltip, Legend, ChartDataLabels);
 
 const BarChart = ({ data, options }) => {
   return <Bar data={data} options={options} />;
