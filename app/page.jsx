bc5662885609b79b3002ac3af771e12e7bcdd622
Brice Roehrig <briceroehrig@Brices-MacBook-Pro-2.local>
2024-10-04 16:57:55 +0400

    feat: add login/sign up form css and tabs

--- app/page.jsx
+++ app/page.jsx
@@ -41,6 +41,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 export const description =
   "A login page with two vertically stacked sections. The first section has the login form with email and password. There's a Forgot your password link and a link to sign up if you do not have an account. The second section has a cover image.";
@@ -50,57 +56,116 @@ export default function Dashboard() {
     <div className="w-full min-h-screen flex flex-col lg:flex-row no-scrollbar">
       {/* First vertical section: Login form */}
       <div className="flex flex-col items-center justify-center py-12 w-full lg:w-1/2 h-screen overflow-y-auto no-scrollbar">
-        <Card className="mx-auto grid w-[350px] gap-6">
-          <CardHeader className="grid gap-2 text-center">
-            <CardTitle className="text-3xl font-bold">Login</CardTitle>
-            <CardDescription className="text-muted-foreground">
-              Please enter your credentials to login to your account.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </CardContent>
-          <CardFooter>
-            <div className="flex items-center">
-              <CardDescription className="text-muted-foreground">
-                Don't have an account?  
-                <Link
-                  href="/"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Sign up
-                </Link>
-              </CardDescription>
-            </div>
-          </CardFooter>
-        </Card>
-      </div>
+        <Tabs defaultValue="Login" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="Login">Login</TabsTrigger>
+            <TabsTrigger value="Sign up">Sign up</TabsTrigger>
+          </TabsList>
+          <TabsContent value="Login">
+            <Card>
+              {/* <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>
+                  Please enter your credentials to login to your account.
+                </CardDescription>
+              </CardHeader> */}
+              <CardContent className="grid gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <Link
+                      href="/forgot-password"
+                      className="ml-auto inline-block text-sm underline"
+                    >
+                      Forgot your password?
+                    </Link>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button>
+              </CardContent>
+              <CardFooter>
+                <div className="flex items-center">
+                  <CardDescription className="text-muted-foreground">
+                    Don't have an account?  
+                    <Link
+                      href="/"
+                      className="ml-auto inline-block text-sm underline"
+                    >
+                      Sign up
+                    </Link>
+                  </CardDescription>
+                </div>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+            <TabsContent value="Sign up">
+              <Card>
+                {/* <CardHeader>
+                  <CardTitle>Create an account</CardTitle>
+                  <CardDescription>
+                    Make changes to your account here. Click save when you're done.
+                  </CardDescription>
+                </CardHeader> */}
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" defaultValue="Pedro Duarte" />
+                  </div>
+                  <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                    </div>
+                    <Input id="password" type="password" required />
+                  </div>
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                    <Button variant="outline" className="w-full">
+                      Login with Google
+                    </Button>
+                </CardContent>
+                  <CardFooter>
+                  <div className="flex items-center">
+                    <CardDescription className="text-muted-foreground">
+                      Already have an account?  
+                      <Link
+                        href="/"
+                        className="ml-auto inline-block text-sm underline"
+                      >
+                        Login
+                      </Link>
+                    </CardDescription>
+                  </div>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </div>
+        
 
       {/* Second vertical section: Cover image */}
       <div className="hidden lg:block bg-muted w-full lg:w-1/2 h-screen overflow-y-auto no-scrollbar">
