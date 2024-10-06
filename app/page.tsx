e15441d3dbcffcbdc1e477da78ec34edc6e80392
ouss <h4r0us@gmail.com>
2024-10-06 12:02:58 +0200

    fix: remove the size of the navbard from h-screen

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ export const description =
 
 const HomePage = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col lg:flex-row no-scrollbar">
+    <div className="w-full min-h-[calc(100vh-2.5rem)] flex flex-col lg:flex-row no-scrollbar">
       {/* First vertical section: Login form */}
       <div 
-        className="flex flex-col items-center justify-center py-12 w-full lg:w-2/5 h-screen overflow-y-auto no-scrollbar bg-astronaut bg-cover bg-center"
+        className="flex flex-col items-center justify-center py-12 w-full lg:w-2/5 h-[calc(100vh-2.5rem)] overflow-y-auto no-scrollbar bg-astronaut bg-cover bg-center"
       >
         <Login />
       </div>
 
-      <div className="bg-slate-900 text-white w-full lg:w-3/5 h-screen overflow-y-auto no-scrollbar scroll-snap-mandatory scroll-snap-y">
+      <div className="bg-slate-900 text-white w-full lg:w-3/5 h-[calc(100vh-2.5rem)] overflow-y-auto no-scrollbar scroll-snap-mandatory scroll-snap-y">
         <div className="space-y-16 p-8">
           <Sections />
         </div>
