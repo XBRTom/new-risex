4d4278adfb0ef2e08476f8ba0639c33b0f2bb3e8
XBRTom <broehrig.contact@gmail.com>
2024-10-14 15:19:57 +0400

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@
 import Login from "@/components/homepage/login";
 import Sections from "@/components/homepage/sections";
 
-export const description =
-  "A login page with two vertically stacked sections. The first section has the login form with email and password. There's a Forgot your password link and a link to sign up if you do not have an account. The second section has a cover image.";
-
 const HomePage = () => {
   return (
     <div className="w-full min-h-[calc(100vh-2.5rem)] flex flex-col lg:flex-row no-scrollbar">
