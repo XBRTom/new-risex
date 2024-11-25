a6b147fa07426f9f8d0c5bcb521604cb50e066d9
ouss <h4r0us@gmail.com>
2024-11-25 22:18:28 +0100

    feat: update loader

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -3,6 +3,36 @@
 import Overview from '@/components/dashboard/Overview';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { cn } from "@/lib/utils"
+
+export interface ISVGProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  className?: string;
+}
+
+const LoadingSpinner = ({
+  size = 24,
+  className,
+  ...props
+}: ISVGProps) => {
+  return (
+      <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("animate-spin", className)}
+      >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+      </svg>
+  );
+};
 
 export default function UserPanel() {
   const { data: session, status } = useSession();
@@ -10,7 +40,7 @@ export default function UserPanel() {
 
   // Handle loading state with a refined loading indicator
   if (status === 'loading') {
-    return <p>Loading...</p>; // Optional: Replace with a loading spinner or animation
+    return <div className="w-full h-screen bg-black text-white flex flex-col gap-2 items-center justify-center"><span>Loading</span><LoadingSpinner className="mr-2" /></div>
   }
 
   // Redirect if unauthenticated, with null return to avoid rendering
--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -3,13 +3,43 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Transactions from '@/components/dashboard/Transactions';
+import { cn } from "@/lib/utils"
+
+export interface ISVGProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  className?: string;
+}
+
+const LoadingSpinner = ({
+  size = 24,
+  className,
+  ...props
+}: ISVGProps) => {
+  return (
+      <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("animate-spin", className)}
+      >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+      </svg>
+  );
+};
 
 export default function TransactionsPage() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
   if (status === 'loading') {
-    return <p>Loading...</p>; // Optional: Replace with a loading spinner for better UX
+    return <div className="w-full h-screen bg-black text-white flex flex-col gap-2 items-center justify-center"><span>Loading</span><LoadingSpinner className="mr-2" /></div>
   }
 
   if (!session && status === 'unauthenticated') {
